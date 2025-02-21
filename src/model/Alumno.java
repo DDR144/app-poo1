ef87e67337508883d39d6028bb4edc3dafb395f8
DDR144 <luis.fernando.didier.hr@gmail.com>
2025-02-20 20:20:40 -0500

    Aplicando polimorfismo a la clase alumno y docente

--- src/model/Alumno.java
+++ src/model/Alumno.java
@@ -1,5 +1,11 @@
 package model;
 
 public class Alumno extends Persona{
-
+    @Override
+    public boolean registrarAsistencia(String codIdentificador, String fechaAsistencia, String horaAsistencia, Boolean valorAsistencia) {
+        System.out.println("Aplicar logica " +
+                "para la asistencia del " +
+                "alumno");
+        return true;
+    }
 }
--- src/model/Docente.java
+++ src/model/Docente.java
@@ -1,5 +1,11 @@
 package model;
 
 public class Docente extends Persona{
-
+    @Override
+    public boolean registrarAsistencia(String codIdentificador, String fechaAsistencia, String horaAsistencia, Boolean valorAsistencia) {
+        System.out.println("aplicar logica " +
+                "para a asistencia del " +
+                "docente");
+        return true;
+    }
 }
